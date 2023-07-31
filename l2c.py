2a238caeb215738aad8eb8670b0d17b34d3c7033
Ahmed-Khaled-Saleh <ahmed_khaled3cs@ci.suez.edu.eg>
2023-07-31 11:43:08 +0300

    latest update

--- l2c.py
+++ l2c.py
@@ -9,12 +9,12 @@ from utils import (local_sgd,
 
 from tqdm import tqdm
 import log
+from copy import deepcopy
 
 
-
-def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_0, K_0):
+def L2C(beta, neighbour_set, train_loaders, val_loaders, test_loaders, S, T, T_0, K_0):
     
-    k = len(neighbour_sets)
+    k = len(neighbour_set)
     device = torch.device("cuda" if not torch.cuda.is_available() else "cpu")
     model = CNNCifar().to(device)
     criterion = nn.CrossEntropyLoss()
@@ -25,10 +25,10 @@ def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_
 
     test_accuracies = [[] for _ in range(k)]
 
-    theta = [model for _ in range(k)]
-    theta_half = [model for _ in range(k)]
-    delta_theta = [model for _ in range(k)]
-    theta_next = [model for _ in range(k)]
+    theta = [CNNCifar().to(device) for _ in range(k)]
+    theta_half = [CNNCifar().to(device) for _ in range(k)]
+    delta_theta = [CNNCifar().to(device) for _ in range(k)]
+    theta_next = [CNNCifar().to(device) for _ in range(k)]
 
     w = torch.zeros(k, k, dtype=theta[0].alpha.dtype, device=theta[0].alpha.device)
 
@@ -53,7 +53,7 @@ def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_
 
                 # Change capturing
                 log.info(f'Computing change capturing at node {i + 1}')
-                import pdb; pdb.set_trace()
+                # import pdb; pdb.set_trace()
                 delta_theta[i].fc1_weight = theta[i].fc1_weight - theta_half[i].fc1_weight.clone()
                 delta_theta[i].fc1_bias = theta[i].fc1_bias - theta_half[i].fc1_bias.clone()
                 delta_theta[i].fc2_weight = theta[i].fc2_weight - theta_half[i].fc2_weight.clone()
@@ -65,29 +65,7 @@ def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_
                 delta_theta[i].conv2_weight = theta[i].conv2_weight - theta_half[i].conv2_weight.clone()
                 delta_theta[i].conv2_bias = theta[i].conv2_bias - theta_half[i].conv2_bias.clone()
                                
-                log.info(f'Computing mixing weights at node {i + 1}')
-                # Mixing weights calculation like this :\highlightcyan{$w_{i,j}=\frac{\exp(\alpha_{i,j})}{\sum_{\ell\in i\cup N(i)}\exp(\alpha_{i,\ell})}$}
-                for j in neighbour_sets[i]:
-                    w[i][j] = torch.exp(theta[i].alpha[i][j])
-                w[i] /= w[i].sum()
-                
-                theta[i].w.data = w
-
-                # Aggregation
-                log.info(f'Aggergating at node {i + 1}')
-
-                for j in neighbour_sets[i]:
-                    theta_next[i].fc1_weight = theta[i].fc1_weight -  theta[i].w[i][j] * delta_theta[i, j].fc1_weight.clone()
-                    theta_next[i].fc1_bias = theta[i].fc1_bias - theta[i].w[i][j] * delta_theta[i, j].fc1_bias.clone()
-                    theta_next[i].fc2_weight = theta[i].fc2_weight - theta[i].w[i][j] * delta_theta[i, j].fc2_weight.clone()
-                    theta_next[i].fc2_bias = theta[i].fc2_bias - theta[i].w[i][j] * delta_theta[i, j].fc2_bias.clone()
-                    theta_next[i].fc3_weight = theta[i].fc3_weight - theta[i].w[i][j] * delta_theta[i, j].fc3_weight.clone()
-                    theta_next[i].fc3_bias = theta[i].fc3_bias - theta[i].w[i][j] * delta_theta[i, j].fc3_bias.clone()
-                    theta_next[i].conv1_weight = theta[i].conv1_weight - theta[i].w[i][j] * delta_theta[i, j].conv1_weight.clone()
-                    theta_next[i].conv1_bias = theta[i].conv1_bias - theta[i].w[i][j] * delta_theta[i, j].conv1_bias.clone()
-                    theta_next[i].conv2_weight = theta[i].conv2_weight - theta[i].w[i][j] * delta_theta[i, j].conv2_weight.clone()
-                    theta_next[i].conv2_bias = theta[i].conv2_bias - theta[i].w[i][j] * delta_theta[i, j].conv2_bias.clone()
-
+                log.info(f'Computing mixing weights at node {i + 1}')               
                 # Update L2C
                 log.info(f'Updating L2C at node {i + 1}')
                 # a training loop to find alpha that minimizes the validation loss
@@ -97,7 +75,13 @@ def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_
                     
                     l2c_optimizer.zero_grad()
                     model.alpha.requires_grad_(True)
-                    outputs = theta_next[i](inputs)
+                    outputs = theta_next[i](inputs,
+                                            val=True,
+                                            neighbour_set=neighbour_set,
+                                            i=i,
+                                            w=w,
+                                            delta_theta=delta_theta[i])
+                    
                     loss = criterion(outputs, labels)
                     loss.backward()
                     print(f'gradient of alpha is {theta_next[i].alpha.grad}')
@@ -106,8 +90,8 @@ def L2C(beta, neighbour_sets, train_loaders, val_loaders, test_loaders, S, T, T_
                 # Remove edges for sparse topology
                 if t == T_0:
                     for _ in range(K_0):
-                        j = min(neighbour_sets[i], key=lambda x: w[i][x])
-                        neighbour_sets[i].delete(j)
+                        j = min(neighbour_set[i], key=lambda x: w[i][x])
+                        neighbour_set[i].delete(j)
 
                 # theta[i] = model.state_dict().copy()
                 # theta_half[i] = model.state_dict().copy()
--- model.py
+++ model.py
@@ -7,34 +7,62 @@ from utils import compute_mixing_weights
 class CNNCifar(nn.Module):
     def __init__(self):
         super(CNNCifar, self).__init__()
-        self.pool = nn.MaxPool2d(2, 2)
 
         self.conv1_weight = torch.randn(6, 3, 5, 5, requires_grad=True)
         self.conv1_bias = torch.randn(6, requires_grad=True)
 
         self.conv2_weight = torch.randn(16, 6, 5, 5, requires_grad=True)
         self.conv2_bias = torch.randn(16, requires_grad=True)
-
+       
         self.fc1_weight = torch.randn(120, 16 * 5 * 5, requires_grad=True)
         self.fc1_bias = torch.randn(120, requires_grad=True)
-
+       
         self.fc2_weight = torch.randn(84, 120, requires_grad=True)
         self.fc2_bias = torch.randn(84, requires_grad=True)
-
+        
         self.fc3_weight = torch.randn(10, 84, requires_grad=True)
         self.fc3_bias = torch.randn(10, requires_grad=True)
-
+        
         self.alpha = nn.Parameter(torch.randn(100, 100), requires_grad=True)
         self.w = nn.Parameter(torch.randn(100, 100), requires_grad=True)
 
+        self.params_list = [self.conv1_weight, self.conv1_bias, self.conv2_weight, self.conv2_bias, self.fc1_weight, self.fc1_bias, self.fc2_weight, self.fc2_bias, self.fc3_weight, self.fc3_bias]
 
-    def forward(self, x):
-        x = self.pool(F.relu(F.conv2d(x, self.conv1_weight, self.conv1_bias)))
-        x = self.pool(F.relu(F.conv2d(x, self.conv2_weight, self.conv2_bias)))
+    def compute_mixing_weights(self, neighbour_set, i, w):
+        for j in neighbour_set[i]:
+            if j != 0:
+                w[i][j] = torch.exp(self.alpha[i][j])
+        w[i] /= w[i].sum()
+        self.w[i].data = w[i]
+
+    def aggregat(self, neighbour_set, i, w, delta_theta):
+        for j in neighbour_set[i]:
+            if j != 0:
+                self.conv1_weight = self.conv1_weight - self.w[i][j] * delta_theta.conv1_weight.clone()
+                self.conv1_bias = self.conv1_bias - self.w[i][j] * delta_theta.conv1_bias.clone()
+                self.conv2_weight = self.conv2_weight - self.w[i][j] * delta_theta.conv2_weight.clone()
+                self.conv2_bias = self.conv2_bias - self.w[i][j] * delta_theta.conv2_bias.clone()
+                self.fc1_weight = self.fc1_weight - self.w[i][j] * delta_theta.fc1_weight.clone()
+                self.fc1_bias = self.fc1_bias - self.w[i][j] * delta_theta.fc1_bias.clone()
+                self.fc2_weight = self.fc2_weight - self.w[i][j] * delta_theta.fc2_weight.clone()
+                self.fc2_bias = self.fc2_bias - self.w[i][j] * delta_theta.fc2_bias.clone()
+                self.fc3_weight = self.fc3_weight - self.w[i][j] * delta_theta.fc3_weight.clone()
+                self.fc3_bias = self.fc3_bias - self.w[i][j] * delta_theta.fc3_bias.clone()
+
+
+    def forward(self, x, val=False, neighbour_set=None, i=None, w=None, delta_theta=None):
+
+        if val:
+            self.compute_mixing_weights(neighbour_set, i, w)
+            self.aggregat(neighbour_set, i, w, delta_theta)
+
+            
+        x = F.max_pool2d(F.relu(F.conv2d(x, self.conv1_weight, self.conv1_bias)), 2, 2)
+        x = F.max_pool2d(F.relu(F.conv2d(x, self.conv2_weight, self.conv2_bias)), 2, 2)
         x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(torch.nn.functional.linear(x, self.fc1_weight, self.fc1_bias))
-        x = F.relu(torch.nn.functional.linear(x, self.fc2_weight, self.fc2_bias))
-        x = torch.nn.functional.linear(x, self.fc3_weight, self.fc3_bias)
+        x = F.relu(F.linear(x, self.fc1_weight, self.fc1_bias))
+        x = F.relu(F.linear(x, self.fc2_weight, self.fc2_bias))
+        x = F.linear(x, self.fc3_weight, self.fc3_bias)
         return F.log_softmax(x, dim=1)
 
 
